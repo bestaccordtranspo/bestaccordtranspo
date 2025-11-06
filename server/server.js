8ef90b157836b1965bfe7b54664bb84718968372
bestaccordtranspo <bestaccordtranspo@gmail.com>
2025-11-06 10:56:01 +0800

    updated cors origin url

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://bestaccord-transportation.vercel.app'
+    'https://bestaccordtranspo.vercel.app'
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
